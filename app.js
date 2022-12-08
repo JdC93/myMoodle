cfbfe8572892e925e00211118dad6c4b62c0f825
JdC93 <91097022+JdC93@users.noreply.github.com>
2022-12-08 02:32:37 -0500

    patch

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ app.use(session({
   secret: "This is a key for the cookie",
   resave: false,
   saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: 'mongodb+srv://admin-parsa:Parsa1234@cluster0.v6lcnxb.mongodb.net/soen287proj' })
+  store: MongoStore.create({ mongoUrl: 'mongodb+srv'+process.env.DBuser+':'+process.env.DBpass+'@cluster0.v6lcnxb.mongodb.net/soen287proj' })
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 
-mongoose.connect("mongodb+srv://admin-parsa:Parsa1234@cluster0.v6lcnxb.mongodb.net/soen287proj", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://"+process.env.DBuser+":"+process.env.DBpass+"@cluster0.v6lcnxb.mongodb.net/soen287proj", {useNewUrlParser: true});
 
 
 
